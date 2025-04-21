c4ba9baa83c83e7e6a336579338c9d8e1e72f0a2
ivolnov <holyloader@gmail.com>
2025-04-21 16:30:55 +0300

    initial commit

---
+++ src/main/java/ua/com/ivolnov/sci_article_library/ScientificArticleLibraryApplication.java
@@ -0,0 +1,13 @@
+package ua.com.ivolnov.sci_article_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificArticleLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScientificArticleLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/ivolnov/sci_article_library/ScientificArticleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.ivolnov.sci_article_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScientificArticleLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
