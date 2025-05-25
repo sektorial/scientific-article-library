af88460b36c73b6a0890d24355282da525643e5c
ivolnov <holyloader@gmail.com>
2025-05-25 23:40:16 +0300

    fix NPE upon article creation

--- service/src/main/java/ua/com/ivolnov/sci_article_library/article/ArticleDto.java
+++ service/src/main/java/ua/com/ivolnov/sci_article_library/article/ArticleDto.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Value;
@@ -35,6 +34,9 @@ class ArticleDto {
     Integer year;
 
     UUID getId() {
+        if (id == null) {
+            return null;
+        }
         return UUID.fromString(id);
     }
 }
